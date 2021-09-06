83b0311ef2faab393fa0921996da655de78af2b9
unknown <joao.pedro.marques@me.com>
2021-09-06 10:41:30 +0100

    bot_token parameter dont required to habe'bot' appended anymore

--- telegram.py
+++ telegram.py
@@ -2,7 +2,7 @@ import requests
 
 
 def telegram_send_message(bot_token, user_id, html):    
-    url = f"https://api.telegram.org/{bot_token}/sendMessage"
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
     payload={'chat_id': user_id, 'text': html, 'parse_mode': 'HTML', 'disable_web_page_preview': True}
 
